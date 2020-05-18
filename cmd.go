ae3b3634af328811c54d00b9984278e2bf6ba37a
CanftIn <wwc7033@gmail.com>
2020-05-18 18:15:03 +0800

    compile to bytecode

--- cmd.go
+++ cmd.go
@@ -6,11 +6,12 @@ import "os"
 
 // java [-options] class [args...]
 type Cmd struct {
-	helpFlag bool
+	helpFlag    bool
 	versionFlag bool
-	cpOption string
-	class string
-	args []string
+	cpOption    string
+	XjreOption  string
+	class       string
+	args        []string
 }
 
 func parseCmd() *Cmd {
@@ -19,9 +20,10 @@ func parseCmd() *Cmd {
 	flag.Usage = printUsage
 	flag.BoolVar(&cmd.helpFlag, "help", false, "print help message")
 	flag.BoolVar(&cmd.helpFlag, "?", false, "print help message")
-	flag.BoolVar(&cmd.versionFlag, "version", false, "print version")
+	flag.BoolVar(&cmd.versionFlag, "version", false, "print version and exit")
 	flag.StringVar(&cmd.cpOption, "classpath", "", "classpath")
 	flag.StringVar(&cmd.cpOption, "cp", "", "classpath")
+	flag.StringVar(&cmd.XjreOption, "Xjre", "", "path to jre")
 	flag.Parse()
 
 	args := flag.Args()
